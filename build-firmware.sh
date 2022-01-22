e60c31b727c8de6e2ef67bb96ebf8ca9aff91d3d
Kay Sievers <kay@vrfy.org>
2022-01-23 00:51:55 +0100

    Update

--- build-firmware.sh
+++ build-firmware.sh
@@ -2,27 +2,22 @@
 
 set -e
 
-if [[ "$1" == "--clean" ]]; then
-  rm -f *.firmware-*
-fi
-
 project=~/github/Spatial-Media-Lab
 
 devices="\
   tracker\
 "
 
-output=index.json
+if [[ -d "$1" ]]; then
+  devices=$(basename "$1")
+fi
 
 rm -rf build/
 mkdir build/
 
-comma=''
-echo '{' > $output
-
 for i in $devices; do
   name=$(basename $i)
-  meta=$(grep V2DEVICE_METADATA $project/$i/$(basename $name).ino)
+  meta=$(grep V2DEVICE_METADATA $project/$i/$name.ino)
   # V2DEVICE_METADATA("com.versioduo.pad", 4, "versioduo:samd:pad")
   match='.*V2DEVICE_METADATA\("(.*)",[[:space:]]([0-9]*),[[:space:]]"(.*)"\).*'
   id=$(echo $meta | sed -E "s/$match/\1/")
@@ -33,26 +28,18 @@ for i in $devices; do
   arduino-cli compile --fqbn $board --output-dir build $project/$i
   mv build/$name.ino.bin build/$firmware
 
-  hash=$(shasum -a 1 build/$firmware | sed -E 's/ .*//')
-
-  echo -ne $comma >> $output
-  comma=",\n"
-
-  echo '  "'$id'": {' >> $output
-  echo '    "file": "'$firmware'",' >> $output
-  echo '    "version": '$version',' >> $output
-  echo '    "board": "'$board'",' >> $output
-  echo '    "hash": "'$hash'"' >> $output
-  echo -n '  }' >> $output
-
   rm -f $id.firmware-$version.uf2
   python ../uf2-samdx1/lib/uf2/utils/uf2conv.py -b 16384 -c -o build/$id.firmware-$version.uf2 build/$firmware
 done
 
-echo -e "\n}" >> $output
+if [[ "$1" == "--clean" ]]; then
+  rm -f *.firmware-*
+fi
 
 chmod 0444 build/*.bin
 mv -f build/*.bin .
 mv -f build/*.uf2 .
 rm -rf build/
-git add index.json *.bin *.uf2
+git add *.firmware-*
+
+./build-index.sh
---
+++ build-index.sh
@@ -0,0 +1,49 @@
+#!/bin/zsh
+
+set -e
+
+project=~/Documents/Arduino/versioduo
+
+# Get the list of devices.
+typeset -A devices
+for i in *.firmware-*.bin; do
+  local meta=$(strings $i | grep '"com.versioduo.firmware"')
+  local id=$(echo $meta | sed -E 's/.*"id":"([^"]+).*/\1/')
+  local version=$(echo $meta | sed -E 's/.*"version":([^,]+).*/\1/')
+  devices[$id]="$id"
+done
+
+output=index.json
+
+echo '{' > $output
+
+# Write the device array.
+local comma=''
+for device versions in "${(@kv)devices}"; do
+  echo -ne $comma >> $output
+  comma=",\n"
+  echo '  "'$device'": [' >> $output
+
+  # Write the firmware versions for this device.
+  local comma2=''
+  for i in $device.firmware-*.bin; do
+    echo -ne $comma2 >> $output
+    comma2=",\n"
+    meta=$(strings "$i" | grep '"com.versioduo.firmware"')
+    id=$(echo $meta | sed -E 's/.*"id":"([^"]+).*/\1/')
+    version=$(echo $meta | sed -E 's/.*"version":([^,]+).*/\1/')
+    board=$(echo $meta | sed -E 's/.*"board":"([^"]+).*/\1/')
+    hash=$(shasum -a 1 $i | sed -E 's/ .*//')
+    echo '    {' >> $output
+    echo '      "file": "'$i'",' >> $output
+    echo '      "version": '$version',' >> $output
+    echo '      "board": "'$board'",' >> $output
+    echo '      "hash": "'$hash'"' >> $output
+    echo -n '    }' >> $output
+  done
+
+  echo -ne '\n  ]' >> $output
+done
+
+echo -e "\n}" >> $output
+
