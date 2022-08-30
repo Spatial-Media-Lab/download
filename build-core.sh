f2af5b0bb037f2abe686497184ac8ae965cdf8b1
Kay Sievers <kay@versioduo.com>
2022-08-30 14:41:09 +0200

    Update core package

--- build-core.sh
+++ build-core.sh
@@ -2,6 +2,10 @@
 
 set -e
 
+if [[ "$1" == "--clean" ]]; then
+  rm -f spatialmedialab-samd-*
+fi
+
 rm -rf arduino-board-package
 git clone --depth=1 git@github.com:Spatial-Media-Lab/arduino-board-package
 (cd arduino-board-package && git submodule update --init --recursive --remote --recommend-shallow)
@@ -9,9 +13,18 @@ git clone --depth=1 git@github.com:Spatial-Media-Lab/arduino-board-package
 rm -rf samd/
 mkdir samd/
 
+# Our board definitions
 cp -a  arduino-board-package/{variants,boards.txt,platform.txt} samd/
+
+# The SAMD core from Adafruit
 cp -a  arduino-board-package/ArduinoCore-samd/cores samd/
+rm -f  samd/cores/arduino/math_helper.*
+
+# Copy only the needed libraries
 cp -aL  arduino-board-package/libraries/ samd/libraries/
+
+# Disable debug, it occupies Serial1.
+sed -i '' 's/#define CFG_TUSB_DEBUG 2/#define CFG_TUSB_DEBUG 0/' samd/libraries/Adafruit_TinyUSB_Arduino/src/arduino/ports/samd/tusb_config_samd.h
 
 version=$(grep 'version=' arduino-board-package/platform.txt | sed -E 's/.*version=([0-9]*).*/\1/')
 
--- build-index.sh
+++ build-index.sh
@@ -44,4 +44,3 @@ for device versions in "${(@kv)devices}"; do
 done
 
 echo -e "\n}" >> $output
-
